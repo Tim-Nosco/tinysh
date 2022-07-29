119c3af3766cb0a65f2e8bf12c7f3a293514d430
jocular <tim@jocular.us>
2022-07-29 12:57:54 -0400

    Add error handling

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ use p256::PublicKey;
 use sha2::Sha256;
 use rand_chacha::ChaCha20Rng;
 use rand_core::{SeedableRng, RngCore, CryptoRng};
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use thiserror::Error;
 
 #[allow(unused_imports)]
 use aes_gcm::{Aes256Gcm, Key, Nonce};
@@ -30,8 +31,18 @@ pub fn open64(pathname: *const i8, oflag: i32) -> i32 {
     unsafe { libc::open(pathname, oflag) }
 }
 
+// Get remote's public key appended the end of this ELF
+fn get_remote_key(filename: &str) -> Result<PublicKey> {
+    // TODO: actually read the final bytes of this file
+    let test_str = "-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEdojljdsw2oUJ/CoGn6p9Bs30yKPd
+pKK0Lb4fC+7c+9lnukYL5WOTsFzfUIZkGdrM5WyoEmDNISrh/mwzAB8m7w==
+-----END PUBLIC KEY-----";
+    Ok(PublicKey::from_str(test_str)?)
+}
+
 // Conduct the ECDH key exchange
-fn play_dh_kex<T: RngCore+CryptoRng, A: Write>(writeable: &mut A, pub_b: PublicKey, rng: &mut T) -> Result<[u8;16]> {
+fn play_dh_kex<T: RngCore+CryptoRng, A: Write>(writeable: &mut A, pub_b: PublicKey, rng: &mut T) -> Result<[u8;32]> {
     // Generate local keys
     let secret_a = EphemeralSecret::random(rng);
     let pub_a = secret_a.public_key().to_string();
@@ -39,25 +50,27 @@ fn play_dh_kex<T: RngCore+CryptoRng, A: Write>(writeable: &mut A, pub_b: PublicK
 
     // Calculate the shared secret
     let secret_hkdf = secret_a.diffie_hellman(&pub_b).extract::<Sha256>(None);
-    let mut key = [0u8;16];
-    secret_hkdf.expand(&vec![0u8;0], &mut key).expect("Unable to expand shared secret");
+    let mut key = [0u8;32];
+    secret_hkdf.expand(&vec![0u8;0], &mut key).or(Err(anyhow!("Unable to expand shared secret")))?;
     Ok(key)
 }
 
+
 #[no_mangle]
-pub fn main(_argc: i32, _argv: *const *const u8) {
+pub fn main(argc: i32, _argv: *const *const u8) {
     // Setup
     let (mut stdout, mut _stdin) = (stdout(), stdin());
     let mut rng = ChaCha20Rng::from_entropy();
-
-    // Decode the remote's public key (appended to this ELF)
-    let test_str = "-----BEGIN PUBLIC KEY-----
-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEdojljdsw2oUJ/CoGn6p9Bs30yKPd
-pKK0Lb4fC+7c+9lnukYL5WOTsFzfUIZkGdrM5WyoEmDNISrh/mwzAB8m7w==
------END PUBLIC KEY-----";
-    let pub_b = PublicKey::from_str(test_str).expect("Failed key parse");
-
+    
+    let pub_b = if argc > 0 {
+        get_remote_key(**_argv)
+    } else {
+        Err(anyhow!("Not enough arguments in argv"))
+    }.expect("Failed to parse remote key");
+    
     // Get the AES key
-    let key = play_dh_kex(&mut stdout, pub_b, &mut rng).expect("Failed KEX");
+    let key = play_dh_kex(
+        &mut stdout, pub_b, &mut rng
+    ).expect("Failed KEX");
     stdout.write(format!("{:#?}", key).as_bytes()).unwrap();
 }
