5e2a1ddd8003bfdaf3c3810b6555f903dc3b0a0d
jocular <tim@jocular.us>
2022-08-05 15:19:19 -0400

    Rename client to local.

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,12 +14,12 @@ cargo +nightly build \
 
 export CC=""
 export CXX=""
-export RUSTFLAGS=""
+#export RUSTFLAGS=""
 cargo +nightly build \
     -Z build-std=std,core,alloc,panic_abort \
     -Z build-std-features=panic_immediate_abort \
     --target x86_64-unknown-linux-gnu \
-    --bin tshc \
+    --bin tshl \
     --release
 
 du -h ./target/mipsel-unknown-linux-uclibc/release/tshr
--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+sudo rm -rf target
+
+sudo service docker start
 docker build -t tinysh:mipsel .
 docker run -it --rm -v $PWD:/opt/tinysh tinysh:mipsel
