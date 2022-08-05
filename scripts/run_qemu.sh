b8ec1da265f5d6681a9ba3ba8a2cf70eea3e372f
jocular <tim@jocular.us>
2022-08-05 17:10:39 -0400

    Fail run if build fails.

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 ./scripts/build.sh
 
 export KEY="MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEdojljdsw2oUJ/CoGn6p9Bs30yKPdpKK0Lb4fC+7c+9lnukYL5WOTsFzfUIZkGdrM5WyoEmDNISrh/mwzAB8m7w=="
