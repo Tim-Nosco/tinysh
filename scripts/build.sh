de4819e7e94b3c49244528e88a5a10aab8e1120a
Jacob Heybey <heybeyja@gmail.com>
2022-08-09 16:24:06 -0400

    Change uclibc build to musl build
    Remove dependence on Docker environment for uClibc toolchain by
    compiling statically using a musl cross compilation toolchain.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# statically linked build
-export RUSTFLAGS='-C target-feature=+crt-static'
+MIPSEL=mipsel-linux-musl-cross.tgz
+if [ ! -f $MIPSEL ]; then
+    wget https://musl.cc/mipsel-linux-musl-cross.tgz && tar xzf mipsel-linux-musl-cross.tgz
+fi
 
-export CC=mipsel-buildroot-linux-uclibc-gcc
-export CXX=mipsel-buildroot-linux-uclibc-g++
 cargo +nightly build \
     -Z build-std=std,core,alloc,panic_abort \
     -Z build-std-features=panic_immediate_abort \
-    --target mipsel-unknown-linux-uclibc \
+    --target mipsel-unknown-linux-musl \
     --bin tshr \
     --release
 
-export CC=""
-export CXX=""
-#export RUSTFLAGS=""
 cargo +nightly build \
     -Z build-std=std,core,alloc,panic_abort \
     -Z build-std-features=panic_immediate_abort \
@@ -22,4 +19,5 @@ cargo +nightly build \
     --bin tshl \
     --release
 
-du -h ./target/mipsel-unknown-linux-uclibc/release/tshr
+du -h ./target/mipsel-unknown-linux-musl/release/tshr
+du -h ./target/x86_64-unknown-linux-gnu/release/tshl
