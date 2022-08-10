7b7c0396544f5e16a248dd9833fc0c669293190b
Tim Nosco <timothy.m.nosco.mil@mail.mil>
2022-08-10 11:46:02 -0400

    Update build process.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,16 +5,12 @@ if [ ! -f $MIPSEL ]; then
     wget https://musl.cc/mipsel-linux-musl-cross.tgz && tar xzf mipsel-linux-musl-cross.tgz
 fi
 
-cargo +nightly build \
-    -Z build-std=std,core,alloc,panic_abort \
-    -Z build-std-features=panic_immediate_abort \
+cargo build \
     --target mipsel-unknown-linux-musl \
     --bin tshr \
     --release
 
-cargo +nightly build \
-    -Z build-std=std,core,alloc,panic_abort \
-    -Z build-std-features=panic_immediate_abort \
+cargo build \
     --target x86_64-unknown-linux-gnu \
     --bin tshl \
     --release
